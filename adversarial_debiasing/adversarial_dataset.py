4de96a0aef126d3ed45f290a08176fe33a577604
Alex Fan <fanafu@gmail.com>
2023-04-19 13:50:08 -0700

    averagemeter

--- adversarial_debiasing/adversarial_dataset.py
+++ adversarial_debiasing/adversarial_dataset.py
@@ -113,6 +113,31 @@ def acc_score(logits, labels):
     num_correct = torch.sum(corrects_bool).item()
     acc = torch.mean(corrects_bool).item()
     return preds, num_correct, 
+    
+class AverageMeter:
+    """Keep track of average values over time.
+    Adapted from:
+        > https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+    def __init__(self):
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def reset(self):
+        """Reset meter."""
+        self.__init__()
+
+    def update(self, val, num_samples=1):
+        """Update meter with new value `val`, the average of `num` samples.
+        Args:
+            val (float): Average value to update the meter with.
+            num_samples (int): Number of samples that were averaged to
+                produce `val`.
+        """
+        self.count += num_samples
+        self.sum += val * num_samples
+        self.avg = self.sum / self.count
 
 def evaluate(model, data_loader, device):
     nll_meter = AverageMeter()
